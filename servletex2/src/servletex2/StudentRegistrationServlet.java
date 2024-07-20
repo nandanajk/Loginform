a65c37e6ff3fe0ea56b09588de906b3a7d164a8b
nandanajk <85984915+nandanajk@users.noreply.github.com>
2024-07-20 14:05:43 +0530

    Add files via upload

---
+++ servletex2/src/servletex2/StudentRegistrationServlet.java
@@ -0,0 +1,51 @@
+package servletex2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentRegistrationServlet
+ */
+@WebServlet("/StudentRegistrationServlet")
+public class StudentRegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentRegistrationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		  response.setContentType("text/html");
+		  String email=request.getParameter("email");
+		  String name=request.getParameter("name");
+		  String password=request.getParameter("password");
+		 
+		  PrintWriter out=response.getWriter();
+		  out.print(email);
+		  out.print(name);
+		  out.print(password);
+	}
+
+}
